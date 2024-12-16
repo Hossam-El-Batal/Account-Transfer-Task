5614e2e065f9cdace67bd55c7f2cbe52b192d129
Hossam Elbatal <tokyo.hossam@gmail.com>
2024-12-17 00:20:24 +0200

    app setup and database schema

---
+++ backend/transactions/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Account(models.Model):
+    id = models.IntegerField(primary_key=True, null=False)
+    name = models.CharField(max_length=255)
+    balance = models.FloatField()
+
+    def __str__(self):
+        return f"{self.id} ({self.name})"
---
+++ backend/transactions/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+import sqlite3
+import pandas as pd
+from transactions.models import Account
+
+class import_data():
+    pass 
+   
